27d6fb92304d0b5ae55f8aea170780c84a17e507
makoto.miyazaki <mktbad57@gmail.com>
2019-05-26 23:00:31 +0900

    make url_sender

--- rtm_connect.py
+++ rtm_connect.py
@@ -1,13 +1,26 @@
 import time
 from slackclient import SlackClient
 import get_env
+import re
 
 slack_token = get_env.Slack_Bots_APIToken
 sc = SlackClient(slack_token)
 
+
 if sc.rtm_connect():
     while sc.server.connected is True:
-        print(sc.rtm_read())
-        time.sleep(1)
+        read_content = sc.rtm_read()
+        if read_content:
+            read_content_text = read_content[0].get('text')
+            subtype = read_content[0].get('subtype')
+            if subtype == 'bot_message':
+                continue
+            if read_content_text:
+                url_part = re.search(
+                    r"(https|http)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)",
+                    read_content_text).group()
+                is_user = read_content[0].get('user')
+                if is_user and url_part:
+                    sc.rtm_send_message("general", url_part)
 else:
     print("Connection Failed")
