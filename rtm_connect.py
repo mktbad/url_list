c9feb7422e1fcd9d4b99f1c6a6cdf5d5e5e5b05a
makoto.miyazaki <mktbad57@gmail.com>
2019-05-26 23:15:18 +0900

    addapt to 19'slack & bug fix

--- rtm_connect.py
+++ rtm_connect.py
@@ -1,4 +1,3 @@
-import time
 from slackclient import SlackClient
 import get_env
 import re
@@ -16,11 +15,13 @@ if sc.rtm_connect():
             if subtype == 'bot_message':
                 continue
             if read_content_text:
-                url_part = re.search(
+                sub = re.search(
                     r"(https|http)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)",
-                    read_content_text).group()
-                is_user = read_content[0].get('user')
-                if is_user and url_part:
-                    sc.rtm_send_message("general", url_part)
+                    read_content_text)
+                if sub is not None:
+                    url_part = sub.group()
+                    is_user = read_content[0].get('user')
+                    if is_user and url_part:
+                        sc.rtm_send_message("url_list", url_part)
 else:
     print("Connection Failed")
